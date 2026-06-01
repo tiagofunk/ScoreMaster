bdc55fd3fdbe6d0e2116ca02788454fb3788d22b
Tiago Funk <tiagoff.tf@gmail.com>
2026-06-01 18:48:17 -0300

    Geração da página de estádios

--- ScoreMaster/src/main/java/funk/tiago/ScoreMaster/controller/HomeController.java
+++ ScoreMaster/src/main/java/funk/tiago/ScoreMaster/controller/HomeController.java
@@ -2,9 +2,17 @@ package funk.tiago.ScoreMaster.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import funk.tiago.ScoreMaster.repository.StadiumRepository;
 
 @Controller
 public class HomeController {
+
+    private final StadiumRepository stadiumRepository;
+
+    public HomeController(StadiumRepository repository) {
+        this.stadiumRepository = repository;
+    }
 
     @GetMapping("/")
     public String homePage() {
@@ -29,5 +37,11 @@ public class HomeController {
     @GetMapping("/createstadium")
     public String createStadiumPage() {
         return "createstadium";
+    }
+
+    @GetMapping("/estadios")
+    public String stadiumsPage(Model model) {
+        model.addAttribute("stadiums", stadiumRepository.findAll());
+        return "stadiums";
     }
 }
